ea3bfb8640a8f598c6e8b9761f35be549af65bea
DBinaghi <39097246+DBinaghi@users.noreply.github.com>
2021-10-15 13:25:30 +0200

    Restyled duplicate warning content, adding information

--- views/admin/check/check.php
+++ views/admin/check/check.php
@@ -1,22 +1,19 @@
 <?php if (!empty($duplicates)): ?>
-    <div id="item_duplicate_check" class="flash">
+    <div id="item_duplicate_check" class="flash alert">
         <?php
-            if (count($duplicates) == 1) {
-                echo __('Duplicate found');
-            } else {
-                echo __('Duplicates found');
-            }
-        ?>:
-        <ul>
+			$countDuplicates = count($duplicates);
+            echo __(plural("Warning: a duplicate of this Item has been found!", "Warning: %s duplicates of this Item have been found!", $countDuplicates), $countDuplicates);
+        ?>
+        <ul style="list-style-type: none; margin-left: 0; padding-left: 0";>
             <?php foreach ($duplicates as $duplicate): ?>
                 <?php $dItem = $duplicate['item']; ?>
                 <?php $dRule = $duplicate['rule']; ?>
                 <li>
-                    <a href="<?php echo url('items') . '/show/' . $dItem->id; ?>"><?php echo metadata($dItem, array('Dublin Core', 'Title')); ?> (#<?php echo $dItem->id; ?>)</a>
-                    <ul>
+                    #<?php echo $dItem->id; ?>: <a href="<?php echo url('items') . '/show/' . $dItem->id; ?>"><?php echo metadata($dItem, array('Dublin Core', 'Title')); ?></a>
+                    <ul style="list-style-type: none";>
                         <?php foreach ($dRule->getElements() as $element): ?>
                             <li>
-                                <?php echo __($element->name) ?>
+                                <b><?php echo __($element->name); ?></b>
                                 <ul>
                                     <?php $texts = metadata($dItem, array($element->getElementSet()->name, $element->name), array('all' => true)); ?>
                                     <?php foreach ($texts as $text): ?>
@@ -30,6 +27,6 @@
             <?php endforeach; ?>
         </ul>
         <?php echo $this->formCheckbox('item_duplicate_check_ignore') ?>
-        <?php echo $this->formLabel('item_duplicate_check_ignore', __('Ignore duplicates')); ?>
+        <?php echo $this->formLabel('item_duplicate_check_ignore', __('Ignore all duplicates when saving')); ?>
     </div>
 <?php endif; ?>
