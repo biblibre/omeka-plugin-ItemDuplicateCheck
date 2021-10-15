fdd44438fec19ad89dce231aa0828046bacbb436
DBinaghi <39097246+DBinaghi@users.noreply.github.com>
2021-10-15 19:02:53 +0200

    Fixed problem with empty elements
    Code in item_duplicate_check_get_duplicates function was showing duplicates where they did not exist, when one or more of the elements included in the rule were empty. This fix solves the issue.

--- ItemDuplicateCheckPlugin.php
+++ ItemDuplicateCheckPlugin.php
@@ -90,10 +90,7 @@ function item_duplicate_check_get_duplicates($item)
 
             foreach ($item['Elements'][$element_id] as $value) {
                 $text = $value['text'];
-                if (0 == strlen(trim($text))) {
-                    continue;
-                }
-                if (function_exists('element_types_format')) {
+                if (0 != strlen(trim($text)) && function_exists('element_types_format')) {
                     $text = element_types_format($element_id, $text);
                 }
 
