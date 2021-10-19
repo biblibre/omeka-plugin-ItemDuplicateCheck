79705f1e706e3c65668894a582f4e1d1c9a7b275
DBinaghi <39097246+DBinaghi@users.noreply.github.com>
2021-10-19 18:53:40 +0200

    Added upgrade, moved out function

--- ItemDuplicateCheckPlugin.php
+++ ItemDuplicateCheckPlugin.php
@@ -1,12 +1,17 @@
 <?php
 
+require_once dirname(__FILE__) . '/helpers/ItemDuplicateCheckFunctions.php';
+
 class ItemDuplicateCheckPlugin extends Omeka_Plugin_AbstractPlugin
 {
     protected $_hooks = array(
         'install',
         'uninstall',
+        'upgrade',
         'initialize',
         'define_acl',
+        'config',
+        'config_form',
         'admin_head',
     );
 
@@ -22,18 +27,47 @@ class ItemDuplicateCheckPlugin extends Omeka_Plugin_AbstractPlugin
                 id int(10) unsigned NOT NULL AUTO_INCREMENT,
                 item_type_id int(10) unsigned NULL DEFAULT NULL,
                 element_ids text NOT NULL,
+                collection_id int(10) unsigned NULL DEFAULT NULL,
                 PRIMARY KEY (id),
                 FOREIGN KEY (item_type_id) REFERENCES {$db->ItemType} (id)
+                    ON DELETE CASCADE ON UPDATE CASCADE,
+                FOREIGN KEY (collection_id) REFERENCES {$db->Collection} (id)
                     ON DELETE CASCADE ON UPDATE CASCADE
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
         ";
         $db->query($sql);
+
+        set_option('item_duplicate_check_list_layout', 'default');
     }
 
     public function hookUninstall()
     {
         $db = $this->_db;
         $db->query("DROP TABLE IF EXISTS {$db->ItemDuplicateCheckRule}");
+
+        delete_option('item_duplicate_check_list_layout');
+    }
+    
+    public function hookUpgrade()
+    {
+        $oldVersion = $args['old_version'];
+        $newVersion = $args['new_version'];
+
+        if (version_compare($oldVersion, '1.1', '<')) {
+            $db = $this->_db;
+            $sql = "
+                ALTER TABLE {$db->ItemDuplicateCheckRule}
+                ADD collection_id int(10) unsigned NULL DEFAULT NULL;
+            ";
+            $db->query($sql);
+
+            $sql = "
+                ALTER TABLE {$db->ItemDuplicateCheckRule}
+                ADD FOREIGN KEY (collection_id) REFERENCES {$db->Collection} (id)
+                    ON DELETE CASCADE ON UPDATE CASCADE
+            ";
+            $db->query($sql);
+        }
     }
 
     public function hookInitialize()
@@ -47,17 +81,28 @@ class ItemDuplicateCheckPlugin extends Omeka_Plugin_AbstractPlugin
         $args['acl']->addResource('ItemDuplicateCheck_Rules');
     }
     
+    public function hookConfig($args)
+    {
+        $post = $args['post'];
+        set_option('item_duplicate_check_list_layout', $post['item_duplicate_check_list_layout']);
+    }
+    
+    public function hookConfigForm()
+    {
+        include 'config_form.php';
+    }
+    
     public function hookAdminHead()
     {
-		$request = Zend_Controller_Front::getInstance()->getRequest();
-		$controller = $request->getControllerName();
-		$action = $request->getActionName();
+        $request = Zend_Controller_Front::getInstance()->getRequest();
+        $controller = $request->getControllerName();
+        $action = $request->getActionName();
 
-		if ($controller == 'items') {
-			queue_js_file('item_duplicate_check');
-			queue_js_string('Omeka.WEB_DIR = ' . js_escape(WEB_DIR) . ';');
-			queue_css_file('item_duplicate_check');
-		}
+        if ($controller == 'items') {
+            queue_js_file('item_duplicate_check');
+            queue_js_string('Omeka.WEB_DIR = ' . js_escape(WEB_DIR) . ';');
+            queue_css_file('item_duplicate_check');
+        }
     }
 
     public function filterAdminNavigationMain($nav)
@@ -69,68 +114,4 @@ class ItemDuplicateCheckPlugin extends Omeka_Plugin_AbstractPlugin
         );
         return $nav;
     }
-}
-
-function item_duplicate_check_get_duplicates($item)
-{
-    $db = get_db();
-    $rules = $db->getTable('ItemDuplicateCheckRule')->findAll();
-
-    $duplicates = array();
-    foreach ($rules as $rule) {
-        if ($rule->item_type_id && $item->item_type_id != $rule->item_type_id) {
-            continue;
-        }
-
-        $elements = $rule->getElements();
-        if (empty($elements)) {
-            continue;
-        }
-
-        $select = $db
-            ->select()
-            ->from(array('i' => $db->Item), array('item_id' => 'id'));
-
-        $joins_added = array();
-        foreach ($elements as $element) {
-            $element_id = $element->id;
-
-            foreach ($item['Elements'][$element_id] as $value) {
-                $text = $value['text'];
-                if (0 != strlen(trim($text)) && function_exists('element_types_format')) {
-                    $text = element_types_format($element_id, $text);
-                }
-
-                if (!isset($joins_added[$element_id])) {
-                    $select->joinLeft(
-                        array("et_$element_id" => $db->ElementText),
-                        "i.id = et_{$element_id}.record_id AND et_{$element_id}.record_type = 'Item' AND et_{$element_id}.element_id = {$element_id}"
-                    );
-                    $joins_added[$element_id] = true;
-                }
-
-                $select->where("et_{$element_id}.text = ?", $text);
-            }
-        }
-
-        $where = $select->getPart(Zend_Db_Select::WHERE);
-        if (empty($where)) {
-            # This will just return the whole items table, abort
-            continue;
-        }
-
-        if (isset($item->id)) {
-            $select->where("i.id != ?", $item->id);
-        }
-        $select->limit(10);
-        $item_ids = $db->fetchCol($select);
-        foreach ($item_ids as $item_id) {
-            $duplicates[] = array(
-                'item' => $db->getTable('Item')->find($item_id),
-                'rule' => $rule,
-            );
-        }
-    }
-
-    return $duplicates;
 }
