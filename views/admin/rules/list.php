740f92b9476b5ee935a2f2736bcf2accab0426ff
DBinaghi <39097246+DBinaghi@users.noreply.github.com>
2021-10-19 18:57:23 +0200

    Added collection column, fixed indentation

--- views/admin/rules/list.php
+++ views/admin/rules/list.php
@@ -5,40 +5,47 @@
 <a href="<?php echo html_escape(url('item-duplicate-check/rules/add')); ?>" class="add full-width-mobile button green"><?php echo __('Add a rule'); ?></a>
 
 <?php if (count($rules)): ?>
-  <table>
-    <thead>
-      <tr>
-        <th><?php echo __('Item Type'); ?></th>
-        <th><?php echo __('Elements'); ?></th>
-        <th><?php echo __('Actions'); ?></th>
-      </tr>
-    </thead>
-    <tbody>
-      <?php foreach ($rules as $rule): ?>
-        <tr>
-          <td>
-            <?php
-              $itemType = $rule->getItemType();
-              echo $itemType ? $itemType->name : '** ' . __('All types') . ' **';
-            ?>
-          </td>
-          <td>
-            <?php
-              $element_names = array();
-              foreach ($rule->getElements() as $element) {
-                $element_names[] = __($element->name);
-              }
-              echo implode(' + ', $element_names);
-            ?>
-          </td>
-          <td>
-            <a href="<?php echo url('item-duplicate-check') . "/rules/edit/rule_id/{$rule->id}"; ?>"><?php echo __('Edit'); ?></a>
-            | <a href="<?php echo url('item-duplicate-check') . "/rules/delete/rule_id/{$rule->id}"; ?>"><?php echo __('Delete'); ?></a>
-          </td>
-        </tr>
-      <?php endforeach; ?>
-    </tbody>
-  </table>
+    <table>
+        <thead>
+            <tr>
+                <th><?php echo __('Item Type'); ?></th>
+                <th><?php echo __('Elements'); ?></th>
+                <th><?php echo __('Collection'); ?></th>
+                <th><?php echo __('Actions'); ?></th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($rules as $rule): ?>
+                <tr>
+                    <td>
+                        <?php
+                            $itemType = $rule->getItemType();
+                            echo $itemType ? $itemType->name : '** ' . __('Any type') . ' **';
+                        ?>
+                    </td>
+                    <td>
+                        <?php
+                            $element_names = array();
+                            foreach ($rule->getElements() as $element) {
+                                $element_names[] = __($element->name);
+                            }
+                            echo implode(' + ', $element_names);
+                        ?>
+                    </td>
+                    <td>
+                        <?php
+                            $collection = $rule->getCollection();
+                            echo $collection ? metadata($collection, 'rich_title', array('no_escape' => true)) : '** ' . __('Any collection') . ' **';
+                        ?>
+                    </td>
+                    <td>
+                        <a href="<?php echo url('item-duplicate-check') . "/rules/edit/rule_id/{$rule->id}"; ?>"><?php echo __('Edit'); ?></a>
+                        | <a href="<?php echo url('item-duplicate-check') . "/rules/delete/rule_id/{$rule->id}"; ?>"><?php echo __('Delete'); ?></a>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
 <?php endif; ?>
 
 <?php echo foot(); ?>
